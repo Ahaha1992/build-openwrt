5a227c59d6de8999e73a8a52ad5d4283c042c713
Ahaha1992 <90120763+Ahaha1992@users.noreply.github.com>
2025-08-22 16:59:27 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -10,7 +10,7 @@
 #
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
-
+sed -i '/^#.*helloworld.git/s/^#//' feeds.conf.default
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
